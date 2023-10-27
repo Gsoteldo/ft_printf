a4ef5a1015f3cef7729f05976af407294ac20ce5
Gabriel <gsoteldor@gmail.com>
2023-10-27 14:59:35 +0200

    27-10-2023(Casa tarde)

--- ft_printf/main.c
+++ ft_printf/main.c
@@ -9,9 +9,10 @@ int main()
    printf("La frase tiene %d letras\n", total);
    ft_printf("%s\n", "Hola mundo");
    ft_printf("El año es %c uando se invento el pan\n");
-    printf("El puntero es %p\n\n", &total);
+    ft_printf("El puntero es %p\n\n", &total);
     printf("El puntero es %x\n\n", 100);
     printf("El puntero es %X\n\n", 100);
     ft_printf("Hola Mundo %c");
+
     return 0;
 }
--- ft_printf/src/ft_printpointer.c
+++ ft_printf/src/ft_printpointer.c
@@ -2,10 +2,31 @@
 
 int ft_printpointer(unsigned long long pointer)
 {
-    
+    int i;
+    char hex[16];
     int counter;
 
-    pointer = 0;
+    i = 0;
     counter = 0;
+    counter += ft_printstring("0x");
+
+    while (pointer != 0)
+    {
+        int temp = 0;
+        temp = pointer % 16;
+        if (temp < 10)
+        {
+            hex[i] = temp + '0';
+            i++;
+        }
+        else
+        {
+            hex[i] = temp -10 + 'a';
+            i++;
+        }
+        pointer = pointer / 16;
+        counter = ft_printstring(hex);
+    }
     return (counter);
 }
+
