f6c520b32e2d80349c8b148adbab794df8e72951
Gabriel Alejandro Soteldo Rodriguez <gsoteldo@c1r12s5.42madrid.com>
2023-11-08 21:56:54 +0100

    Se inicia el cambio de printf para norminette

--- ft_printf/src/ft_printf.c
+++ ft_printf/src/ft_printf.c
@@ -1,5 +1,26 @@
 #include "../include/ft_printf.h"
 
+int ft_type_print(char c, va_list *vargs, int *total)
+{
+    if (c == 'c')
+        total = total + ft_printchar(va_arg(*vargs, int));  
+    else if (c == 's')
+        total = total + ft_printstring(va_arg(*vargs, char *));
+    else if (c == 'i' || c == 'd')
+        total = total + ft_printinteger(va_arg(*vargs, int));
+    else if (c == 'p')
+        total = total + ft_printpointer(va_arg(*vargs, unsigned long long));
+    else if (c == 'x' || c == 'X')
+        total = total + ft_printhexa(va_arg(*vargs, unsigned int), c);
+    else if (c == 'u')
+        total = total + ft_printunsigned(va_arg(*vargs, unsigned int));
+    else if (c == '%')
+        total = total + ft_printchar('%');
+    else
+        total = total + ft_printchar(c);        
+    return (*total);
+}
+
 int ft_printf(const char *string, ...)
 {
     int total;
@@ -13,46 +34,10 @@ int ft_printf(const char *string, ...)
     {
         if(string[i] == '%')
         {
-            if (string[i+1] == 'c')
-            {
-                i++;
-                total = total + ft_printchar(va_arg(vargs, int));
-            }
-            else if (string[i+1] == 's')
-            {
-                i++;
-                total = total + ft_printstring(va_arg(vargs, char *));
-            }
-            else if (string[i+1] == 'i' || string[i+1] == 'd')
-            {
-                i++;
-                total = total + ft_printinteger(va_arg(vargs, int));
-            }
-            else if (string[i+1] == 'p')
-            {
-                i++;
-                total = total + ft_printpointer(va_arg(vargs, unsigned long long));
-
-            }
-            else if (string[i+1] == 'x' || string[i+1] == 'X')
-            {
-                i++;
-                total = total + ft_printhexa(va_arg(vargs, unsigned int), string[i]);
-            }
-            else if (string[i+1] == 'u')
-            {
-                i++;
-                total = total + ft_printunsigned(va_arg(vargs, unsigned int));
-            }
-            else if (string[i+1] == '%')
-            {
-                i++;
-                total = total + ft_printchar(string[i]);
-            }
+            i++;
+            total = total + ft_type_print(string[i], &vargs, &total);
+            i++;
         }
-        else
-           total = total + ft_printchar(string[i]);
-
         i++;
     }
     va_end(vargs);
